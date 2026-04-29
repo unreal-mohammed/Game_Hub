7fc34d1eaa214f7a9a2f4edd7f629f4dc4d2b905
Siefamro <siefamroo106@gmail.com>
2026-04-29 14:06:40 +0300

    change extension into php and make a file of database connection with php

---
+++ dbconn.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydb";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
+
